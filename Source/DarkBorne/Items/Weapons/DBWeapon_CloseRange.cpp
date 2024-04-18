536bba85b081f8a879ee7a73bf2c63285b8ddb56
carttman <93562297+carttman@users.noreply.github.com>
2024-04-18 13:04:18 +0900

    단검 공격 처리 수정, 망토 에셋 수정, blood effect 수정

--- Source/DarkBorne/Items/Weapons/DBWeapon_CloseRange.cpp
+++ Source/DarkBorne/Items/Weapons/DBWeapon_CloseRange.cpp
@@ -15,19 +15,22 @@ ADBWeapon_CloseRange::ADBWeapon_CloseRange()
 	CapsuleComp->SetCapsuleRadius(3);
 
 	CapsuleComp->OnComponentBeginOverlap.AddDynamic(this, &ADBWeapon_CloseRange::OnOverlapBegin);
-	CapsuleComp->OnComponentEndOverlap.AddDynamic(this, &ADBWeapon_CloseRange::OnOverlapEnd);
 }
 
 void ADBWeapon_CloseRange::BeginPlay()
 {
-
+	CapsuleComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 void ADBWeapon_CloseRange::OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	ADBRogueCharacter* RoguePlayer = Cast<ADBRogueCharacter>(OtherActor);
+	//내가 아닌 다른 로그 플레이어를 otherActor로 캐스팅
+	ADBRogueCharacter* OtherPlayer = Cast<ADBRogueCharacter>(OtherActor);
 
-	UDBRogueAnimInstance* RogueAnim = Cast<UDBRogueAnimInstance>(RoguePlayer->GetMesh()->GetAnimInstance());
+	UE_LOG(LogTemp, Warning, TEXT("Testing here: %s"), *GetNameSafe(GetOwner()));
+
+	// 캐릭터의 GetOnwer로 인스턴스를 가져와 나의 플레이어 애님 인스턴스로 가져온다
+	UDBRogueAnimInstance* MyCharacterAnim = Cast<UDBRogueAnimInstance>(Cast<ACharacter>(GetOwner())->GetMesh()->GetAnimInstance());
 
 	// 만약 내 자신이 부딫혔다면
 	if (OtherActor == GetOwner())
@@ -37,36 +40,14 @@ void ADBWeapon_CloseRange::OnOverlapBegin(class UPrimitiveComponent* OverlappedC
 	// 만약 내 자신이 아닌 액터가 부딫혔다면
 	else if (OtherActor != GetOwner())
 	{
-		
-		//Attack anim notify Start
-		RogueAnim->AnimNotify_Start_Damage();
-
 		// 공격중이면
-		if (RogueAnim->isAttacking)
+		if (MyCharacterAnim->isAttacking)
 		{	
-			//플레이어의 현재 체력에서 무기데미지만큼 데미지
-			RoguePlayer->CurrHP = RoguePlayer->CurrHP - WeaponDamage;
-			UE_LOG(LogTemp, Warning, TEXT("%.f"), RoguePlayer->CurrHP);
-			
+			//플레이어의 현재 체력에서 무기데미지만큼 데미지를 준다
+			OtherPlayer->CurrHP = OtherPlayer->CurrHP - WeaponDamage;
+			UE_LOG(LogTemp, Warning, TEXT("%.f"), OtherPlayer->CurrHP);
+			CapsuleComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
 		}
-	}
-}
-
-void ADBWeapon_CloseRange::OnOverlapEnd(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
-{
-	ADBRogueCharacter* RoguePlayer = Cast<ADBRogueCharacter>(GetOwner());
-	UDBRogueAnimInstance* RogueAnim = Cast<UDBRogueAnimInstance>(RoguePlayer->GetMesh()->GetAnimInstance());
-
-	if (OtherActor == GetOwner())
-	{
-
-	}
-	else if (OtherActor != GetOwner())
-	{
-		
-		//Attack Anim Notify End
-		RogueAnim->AnimNotify_End_Damage();
-
-		
 	}
 }
